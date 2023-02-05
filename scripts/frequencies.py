d6cefceb67c937f6348f8e025d536377943a7fce
PastelBelem8 <catarina.belem@ist.utl.pt>
2023-02-05 07:44:18 -0800

    Update frequencies.py: rename unigram_count --> count

--- scripts/frequencies.py
+++ scripts/frequencies.py
@@ -13,21 +13,24 @@ class PositionalFrequencies:
         self.total_tokens = 0
         self.counts = defaultdict(default_init)
 
+    def __add__(self, counts):
+        raise NotImplemented
+
     def add(self, token, pos):
         self.counts[token][pos] += 1
         self.total_tokens += 1
 
-    def unigram_count(self) -> Counter:
-        unigram_count = {}
+    def count(self) -> Counter:
+        count = {}
 
         for token, pos_counts in self.counts.items():
-            unigram_count[token] = sum(pos_counts.values())
+            count[token] = sum(pos_counts.values())
 
-        return Counter(unigram_count)
+        return Counter(count)
 
     def most_common(self, n: int=None, pos=None):
         if pos is None:
-            return self.unigram_count().most_common(n)
+            return self.count().most_common(n)
 
         # If a position is specified, order by position counts
         counts = {}
