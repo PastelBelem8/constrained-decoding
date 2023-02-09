86fa2458645c179110c46e59c58712e507bcd337
PastelBelem8 <catarina.belem@ist.utl.pt>
2023-02-09 15:40:39 -0800

    Fix assertions in SamplingOutput

--- src/sampling/data_objects.py
+++ src/sampling/data_objects.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 
 
 class SamplingOutput:
@@ -13,22 +13,44 @@ class SamplingOutput:
         if output == None:
             return self
 
-        assert self.description == output.description
-        assert self.terms_ids == output.terms_ids
-        assert len(self.probs) == len(output.probs)
-        assert len(self.samples) == len(output.samples)
-        assert len(self.logits) == len(output.logits)
+        assert self.description == output.description, f"{self.description} vs {output.description}"
+        assert np.array_equal(self.terms_ids, output.terms_ids), f"{self.terms_ids} vs {output.terms_ids}"
+        # -------------------------------------------------------------------------------------------
+        # May not be true. Samples unlike probs and logits are not tracked on a decoding basis.
+        # In fact, they represent a single tensor of size batch_size x max_num_tokens.
+        # In rare cases, it may happen that all sampled sequences finish earlier than the specified
+        # number of tokens. Therefore making these comparisons somewhat irrelevant.
+        # -------------------------------------------------------------------------------------------
+        # assert len(self.samples) == len(output.samples), f"{len(self.samples)} vs {len(output.samples)}"
+        # assert len(self.probs) == len(output.probs), f"{len(self.probs)} vs {len(output.probs)}"
+        # assert len(self.logits) == len(output.logits), f"{len(self.logits)} vs {len(output.logits)}"
+        assert len(self.probs) == self.samples.shape[1], f"Error: len(probs) vs shape(samples, 1): {len(self.probs)} vs {self.samples.shape[1]}"
+        assert len(output.probs) == output.samples.shape[1], f"Error: len(probs) vs shape(samples, 1): {len(output.probs)} vs {output.samples.shape[1]}"
+
+        assert len(self.logits) == 0 or len(self.probs) == len(self.logits), f"Error: len(probs) vs len(logits): {len(self.probs)} vs {len(self.logits)}"
+        assert len(output.logits) == 0 or len(output.probs) == len(output.logits), f"Error: len(probs) vs len(logits): {len(output.probs)} vs {len(output.logits)}"
 
         new_probs = []
-        new_samples = []
         new_logits = []
 
-        for i in range(len(self.probs)):
-            new_probs.append(torch.vstack((self.probs[i], output.probs[i])))
-            new_samples.append(torch.vstack((self.samples[i], output.samples[i])))
+        n = min(len(self.probs), len(output.probs))
+        max_seq_size = max(len(self.probs), len(output.probs))
+
+        for i in range(n):
+            new_probs.append(np.vstack((self.probs[i], output.probs[i])))
+            # new_samples.append(torch.vstack((self.samples[i], output.samples[i])))
 
             if len(self.logits) > 0:
-                new_logits.append(torch.vstack((self.logits[i], output.logits[i])))
+                new_logits.append(np.vstack((self.logits[i], output.logits[i])))
+
+        if max_seq_size - n > 0:
+            # TODO
+            # Need to handle new_probs (set them to 1 -- indicating seqs are finished)
+            # Need to handle logits (set them to 1 -- uniform distribution)
+            # Need to handle samples:
+            raise NotImplemented
+        else:
+            new_samples = np.vstack((self.samples, output.samples))
 
         return SamplingOutput(
             probs=new_probs,
