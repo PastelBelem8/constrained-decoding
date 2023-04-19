6c154d7b40b4950d544dc30575370783ebb68026
PastelBelem8 <catarina.belem@ist.utl.pt>
2023-04-18 21:17:45 -0700

    [experiments-ro-prompt] Add control - uncondition generation scripts

---
+++ experiments/experiments-ro-prompt/control/generate.py
@@ -0,0 +1,152 @@
+from sampling.utils import create_history
+from typing import List
+
+import pandas as pd
+import numpy as np
+import torch
+import tqdm
+
+
+def generate(
+    prefix: str, num_sequences: int, batch_size: int, model, tokenizer, device, seed=None, **sampling_kwargs,
+):
+    if seed is not None:
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+    default_kwargs = dict(
+        return_dict_in_generate=True,
+        output_scores=True,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    default_kwargs.update(sampling_kwargs)
+
+    seqs = []
+    seq_scores = []
+    seq_trans_scores = []
+    seq_entr_scores = []
+    for start in range(0, num_sequences, batch_size):
+        size = min(batch_size, num_sequences-start)
+
+        input_ids = (
+            tokenizer(
+                # prefix, return_tensors="pt", add_special_tokens=False
+                tokenizer.bos_token + prefix, return_tensors="pt", add_special_tokens=False
+            ).input_ids
+            if prefix is not None
+            else None
+        )
+        # input_ids = create_history(size, prefix, tokenizer.bos_token_id).to(device)
+        input_ids = create_history(size, input_ids, tokenizer.bos_token_id).to(device)
+        attention_mask = torch.ones_like(input_ids).to(device)
+
+        # Generate sequences
+        outputs = model.generate(input_ids, attention_mask=attention_mask, **default_kwargs)
+        sequences = outputs.sequences
+
+        # Compute each sequence probability
+        results = model(sequences, attention_mask=torch.ones_like(sequences), labels=sequences)
+        batch_score = -results.loss.cpu().detach().numpy()
+
+        # Based on the discussion at
+        # https://discuss.huggingface.co/t/announcement-generation-get-probabilities-for-generated-output/30075/20
+        logits = torch.log_softmax(results.logits, dim=-1).detach()
+
+        # collect the probability of the generated token
+        # -- probability at index 0 corresponds to the token at index 1
+        logits, input_ids = logits[:, :-1, :], sequences[:,1:,None]
+
+        # Scores per token of the template
+        batch_seq_scores = torch.gather(logits, 2, input_ids).squeeze(-1)
+
+        _avg_loss = batch_seq_scores.mean(dim=-1).mean().item()
+        assert np.abs(_avg_loss - batch_score) <= 1e-5, f"Loss does not match: (batch: {input_ids})), {_avg_loss} - {batch_score} > 1e-6"
+
+        seqs.extend(sequences.detach().cpu().numpy().tolist())
+        seq_scores.extend(batch_seq_scores.sum(dim=-1).detach().cpu().numpy().tolist())
+        seq_trans_scores.extend(batch_seq_scores.cpu().detach().numpy())
+
+        # Compute entropy
+        probits = torch.softmax(logits, dim=-1)
+        torch_entropy = -1 * torch.mul(probits, logits).sum(dim=-1)
+        seq_entr_scores.extend(torch_entropy.detach().cpu().detach().numpy())
+
+    return seqs, seq_scores, seq_trans_scores, seq_entr_scores
+
+
+def batch_generate(dump_freq: int, num_sequences: int, sampling_kwargs, batch_size: int, model, tokenizer, seed: int, output_path="./temp.csv", **kwargs) -> dict:
+    results = {"sampling_kwargs": [], "sequence_log_prob": [], "sequence": [], "seq_trans_log_probs": [], "seq_entropy_log_probs": []}
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    pd.DataFrame(results).to_csv(output_path) # overwrite existing filepath
+
+    for num_seqs in tqdm.tqdm(range(0, num_sequences, dump_freq), desc=str(sampling_kwargs)):
+        num_seqs = min(dump_freq, num_sequences - num_seqs)
+
+        sampled_seq, sampled_scores, sampled_seq_trans_scores, sampled_seq_entr_scores = generate(
+            prefix=None, num_sequences=num_seqs, batch_size=batch_size,
+            model=model, tokenizer=tokenizer,
+            **sampling_kwargs,
+            **kwargs,
+        )
+        results["sampling_kwargs"].extend([sampling_kwargs] * len(sampled_seq))
+        results["sequence_log_prob"].extend(sampled_scores)
+        results["sequence"].extend(tokenizer.batch_decode(sampled_seq, skip_special_tokens=True))
+        results["seq_trans_log_probs"].extend(sampled_seq_trans_scores)
+        results["seq_entropy_log_probs"].extend(sampled_seq_entr_scores)
+
+        print("Writing down", len(sampled_seq), "to file:", output_path)
+        pd.DataFrame(results).to_csv(output_path, mode="a", header=False) # overwrite existing filepath
+        results = {"sampling_kwargs": [], "sequence_log_prob": [], "sequence": [], "seq_trans_log_probs": [], "seq_entropy_log_probs": []}
+
+    return results
+
+
+def multinomial_generation(**kwargs):
+    sampling_kwargs = dict(do_sample=True, num_beams=1)
+
+    results = batch_generate(sampling_kwargs=sampling_kwargs, **kwargs)
+
+    return pd.DataFrame(results)
+
+def top_k_sampling(params: List[int], **kwargs):
+    if isinstance(params, int):
+        params = [params]
+
+    results = []
+    for k in params:
+        sampling_kwargs = dict(do_sample=True, top_k=k)
+        result = batch_generate(sampling_kwargs=sampling_kwargs, **kwargs)
+        results.append(result)
+
+    return pd.concat([pd.DataFrame(r) for r in results])
+
+
+def top_p_sampling(params: List[float], **kwargs):
+    if isinstance(params, int):
+        params = [params]
+
+    results = []
+    for p in params:
+        sampling_kwargs = dict(do_sample=True, top_p=p)
+        result = batch_generate(sampling_kwargs=sampling_kwargs, **kwargs)
+        results.append(result)
+
+    results = pd.concat([pd.DataFrame(r) for r in results])
+    return results
+
+
+def temp_sampling(params: List[float], **kwargs):
+    if isinstance(params, (float, int)):
+        params = [params]
+
+    results = []
+    for t in params:
+        sampling_kwargs = dict(do_sample=True, temperature=t)
+        result = batch_generate(sampling_kwargs=sampling_kwargs, **kwargs)
+        results.append(result)
+
+    results = pd.concat([pd.DataFrame(r) for r in results])
+    return results
---
+++ experiments/experiments-ro-prompt/control/run_decoder.py
@@ -0,0 +1,152 @@
+from generate import top_p_sampling, top_k_sampling, temp_sampling, multinomial_generation
+from functools import partial
+from typing import Tuple
+
+import os
+import torch
+import time
+
+def parse_arguments() -> dict:
+    import argparse, yaml
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", required=True, type=str)
+    parser.add_argument("-m", "--model-name", required=True, type=str)
+    parser.add_argument("-r", "--model-revision", default=None, type=str)
+    parser.add_argument("-d", "--device", default="cuda", type=str)
+    parser.add_argument("-bs", "--batch_size", default=None, type=int)
+
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    config["model"] = {
+        "name": args.model_name,
+        "revision": args.model_revision,
+        "device": args.device,
+    }
+
+    if args.batch_size is not None:
+        config["sampling"].update(batch_size=args.batch_size)
+
+    return config
+
+
+def get_model_filename(*args) -> str:
+    """Given a set of strings characterizing the model, create a filename."""
+    args = [a for a in args if a]
+    args = [a.replace("/", "__") for a in args]
+    args = [a for a in args if a]
+    return "__".join(args)
+
+
+def load_model(name, revision=None, device=None) -> Tuple[str, object, object, str]:
+    from transformers import AutoTokenizer
+    def update_model_and_tokenizer(model, tokenizer): pass
+
+    model_kwargs = {}
+    tokenizer_kwargs = {}
+    # Load GPT2 model
+    if "gpt2" in name:
+        from transformers import GPT2LMHeadModel
+        model_class = GPT2LMHeadModel
+
+        def update_model_and_tokenizer(model, tokenizer):
+            tokenizer.pad_token = tokenizer.eos_token
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+            model.config.pad_token_id = model.config.eos_token_id
+
+    elif "gpt-neo" in name:
+        from transformers import GPTNeoForCausalLM
+        model_class = GPTNeoForCausalLM
+
+        def update_model_and_tokenizer(model, tokenizer):
+            tokenizer.pad_token = tokenizer.eos_token
+            model.config.pad_token_id = model.config.eos_token_id
+
+    elif "pythia" in name:
+        # GPTNeoXTokenizerFast
+        from transformers import GPTNeoXForCausalLM
+        model_class = GPTNeoXForCausalLM
+        if revision:
+            model_kwargs.update(revision=revision)
+        tokenizer_kwargs.update(padding_side="left")
+    else:
+        raise ValueError(f"Undefined: {name}")
+
+    model = model_class.from_pretrained(name)
+    tokenizer = AutoTokenizer.from_pretrained(name, **tokenizer_kwargs)
+    update_model_and_tokenizer(model, tokenizer)
+
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model_filename = get_model_filename(name, revision)
+
+    model.to(device)
+    return model_filename, model, tokenizer, device
+
+
+def load_decoding_algorithm(method, params=None, **kwargs) -> Tuple[str, str]:
+    default_params = {}
+    default_params.update(**kwargs)
+
+    if method.lower() in ("top-p", "top_p", "topp"):
+        decoding_name = "top_p"
+        default_params.update(params=params)
+        decoding_method = top_p_sampling
+
+    elif method.lower() in ("top-k", "top_k", "topk"):
+        decoding_name = "top_k"
+        default_params.update(params=params)
+        decoding_method = top_k_sampling
+
+    elif method.lower() in ("temp", "temperature"):
+        decoding_name = "temperature"
+        default_params.update(params=params)
+        decoding_method = temp_sampling
+
+    elif method.lower() in ("multinomial", "random"):
+        decoding_name = "multinomial"
+        decoding_method = multinomial_generation
+
+    else:
+        raise ValueError(f"Unknown decoding algorithm: {method} with params {params}")
+
+    decoding_method = partial(
+        decoding_method,
+        **default_params,
+    )
+
+    return decoding_name, decoding_method
+
+
+if __name__ == "__main__":
+    config = parse_arguments()
+    print(f"Starting Experiment\n[Experiment] Configs: {config}")
+
+    sampling_configs = config.pop("sampling")
+    output_dir = sampling_configs.pop("output_dir")
+    os.makedirs(output_dir, exist_ok=True)
+
+    model_configs = config.pop("model")
+    model_name, model, tokenizer, device = load_model(**model_configs)
+    print("Cuda available:", torch.cuda.is_available())
+
+    decoding_configs = config.pop("decoding")
+    decoding_name, decoding_method = load_decoding_algorithm(**decoding_configs, **sampling_configs)
+
+    output_fp = f"{output_dir}/{model_name}-{decoding_name}.csv"
+    if os.path.exists(output_fp):
+        print(output_fp, "already exists!")
+        os.remove(output_fp)
+
+    from transformers.utils import logging
+    logging.set_verbosity_error() # not the best way
+
+    start = time.time()
+    decoding_method(model=model, tokenizer=tokenizer, device=device, output_path=output_fp)
+    end = time.time()
+
+    print("Decoding duration:", (end - start) / 3600, "h")
+    print("Created file", output_fp)
